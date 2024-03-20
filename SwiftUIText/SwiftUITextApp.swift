f69d5fd1809fecfe4a02f3c43d6448b1a6c851b8
Alex Nagy <rebeloperalpar@gmail.com>
2024-03-20 21:29:48 +0200

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Alex Nagy on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
